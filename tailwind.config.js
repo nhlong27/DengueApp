bae0aecbd2794455a2da3d8b13a5dfd787785af0
Nguyen Hoang Long <long.nguyen2706@hcmut.edu.vn>
2022-11-26 19:46:04 +0700

    setup: add eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  mode: 'jit', // <--- enable JIT
+  purge: ['./src/**/*.{vue,js,ts,jsx,tsx,html}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
